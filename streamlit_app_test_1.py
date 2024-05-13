b395b26b6c02a84002bc573695d05235f6c13fca
Sheep124 <92930676+Sheep124@users.noreply.github.com>
2024-05-13 17:44:33 +0800

    Update streamlit_app_test_1.py

--- streamlit_app_test_1.py
+++ streamlit_app_test_1.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from googletrans import Translator
 
 prodata = pd.read_csv('processed_data.csv')
 selected_column = ['lemma_text','Sentiment_Rating']
@@ -15,9 +18,7 @@ st.write('Hello, *World!* :sunglasses:')
 prodata.dropna(subset=['lemma_text', 'Sentiment_Rating'], inplace=True)
 
 
-import streamlit as st
-from sklearn.feature_extraction.text import TfidfVectorizer
-from googletrans import Translator
+
 
 
 finalData = pd.read_csv('Contraction Review & Sentiment Rating.csv')
