8244e1d437bdc82ae25b96e0299b6f02a1ba4c45
Sheep124 <92930676+Sheep124@users.noreply.github.com>
2024-05-14 00:14:05 +0800

    Update streamlit_app_test_1.py

--- streamlit_app_test_1.py
+++ streamlit_app_test_1.py
@@ -4,10 +4,82 @@ from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from googletrans import Translator
+import emoji
+import re
+import contractions
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from googletrans import Translator
+from nltk.stem import WordNetLemmatizer
 
 data = pd.read_csv('clean_dataset.csv')
 prodata = pd.read_csv('processed_data.csv')
 prodata.dropna(subset=['lemma_text', 'Sentiment_Rating'], inplace=True)
+
+def convert_demojis(text):
+    text_temp = emoji.demojize(text)
+    return text_temp.replace(":", " ")
+
+def remove_punctuation(text):
+    punctuation_pattern = r'[^\w\s]'
+    text_without_punctuation = (re.sub(punctuation_pattern, ' ', text)).lower()
+    
+    cleaned_text_temp = text_without_punctuation.replace('\n', ' ')
+    
+    punctuation_chars = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+    cleaned_text = ''.join(char for char in cleaned_text_temp if char not in punctuation_chars)
+    return cleaned_text
+
+def expand_contractions(text):
+    return contractions.fix(text)
+
+def translate_text_deploy(text, source_lang, target_lang):
+    try:
+        # Initialize the translator
+        translator = Translator()
+
+        # Translate text
+        translation = translator.translate(text, src=source_lang, dest=target_lang)
+
+        return translation.text
+    except Exception as e:
+        return str(e)
+
+def remove_stopwords(text):
+    # Tokenize the text
+    text = str(text)
+    tokens = nltk.word_tokenize(text)
+    # Remove stopwords
+    stopwords_removed = [word for word in tokens if word.lower() not in stopwords.words('english')]
+    # Join the words back into a single string
+    return ' '.join(stopwords_removed)
+
+def word_tokenize_wrapper(text):
+    text = str(text)
+    return word_tokenize(text)
+
+def normalized_term(document):
+    return [normalized_word_dict[term] if term in normalized_word_dict else term for term in document]
+
+def stemming(text):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    do = []
+    for w in text:
+        dt = stemmer.stem(w)
+        do.append(dt)
+    d_clean=[]
+    d_clean=" ".join(do)
+    print(d_clean)
+    return d_clean
+
+
+
+def lemmatize_text(words):
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+    return ' '.join(lemmatized_words)
 
 selected_column = ['lemma_text','Sentiment_Rating']
 prodata.columns
@@ -43,7 +115,7 @@ y = prodata['Sentiment_Rating']
 
 x_train_tfidf, x_test_tfidf, y_train, y_test = train_test_split(x,y , test_size=0.2, random_state=42)
 
-rf_classifier_tfidf = RandomForestClassifier(**{'max_depth': 80, 'max_features': 'sqrt', 'n_estimators': 100})
+rf_classifier_tfidf = RandomForestClassifier(**{'max_depth': 80, 'max_features': 'log2', 'n_estimators': 300})
 
 # Initialize the translator object
 translator = Translator()
@@ -60,12 +132,20 @@ rf_predictions_tfidf = rf_classifier_tfidf.predict(x_test_tfidf)
 def analyze_sentiment(review):
     if review:
         # Translate the review to Malay
-        translation_ms = translator.translate(review, dest='ms')
-        translation_en = translator.translate(translation_ms.text, dest='en')
-        translated_review = translation_en.text
+        review_demoji = convert_demojis(review)
+        punct_text    = remove_punctuation(review_demoji)
+        contra_text   = expand_contractions(punct_text)
+        review_toMs   = translate_text_deploy(contra_text,'en','ms')
+        review_toEn   = translate_text_deploy(review_toMs,'ms','en')
+        removed_review= remove_stopwords(review_toEn)
+        token_text    = word_tokenize_wrapper(removed_review)
+        nomalise_text = normalized_term(token_text)
+        
+        stemming_text = stemming(nomalise_text)
+        lemma_text    = lemmatize_text(nomalise_text)
 
         # Vectorize the translated review
-        review_vectorized = tfidf_vectorizer.transform([translated_review])
+        review_vectorized = tfidf_vectorizer.transform([lemma_text])
 
         # Predict sentiment and get probability estimates
         sentiment = rf_classifier_tfidf.predict(review_vectorized)[0]
